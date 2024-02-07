2bd1d151ca177003371789c8a57101734aa299d4
foxx <james.k@konnektive.com>
2024-02-07 17:19:15 -0500

    .

--- src/api_keys/mod.rs
+++ src/api_keys/mod.rs
@@ -1,5 +1,6 @@
 use std::fs;
-use std::io::BufRead;
+use std::fs::OpenOptions;
+use std::io::{BufRead, Write};
 use std::sync::Mutex;
 use actix_web::HttpRequest;
 use actix_web::web::Data;
@@ -15,8 +16,9 @@ pub async fn create_api_key(
     if req.headers().get("x-api-key").is_some() {
         if is_key_valid(req.headers().get("x-api-key").unwrap().to_str().unwrap().to_string(), data.clone().lock().unwrap().api_key.lock().unwrap().to_vec()) {
             let mut rng = rand::thread_rng();
-            let y: u64 = rng.gen(); // generates a float between 0 and 1
-            y.to_string()
+            let new_key: u64 = rng.gen(); // generates a new api-key
+            add_api_key_to_file(new_key.to_string());
+            new_key.to_string()
         } else {
             "invalid api key\n".to_string()
         }
@@ -33,6 +35,16 @@ pub fn load_keys_from_file() -> Vec<String> {
     }
     // println!("{:#?}", keys);
     keys
+}
+
+fn add_api_key_to_file(new_key: String) {
+    let new_key_formatted = format!("{}\n", new_key);
+    let mut opt = OpenOptions::new()
+        .write(true)
+        .append(true)
+        .open("config/api_keys")
+        .unwrap();
+    opt.write(new_key_formatted.as_bytes()).unwrap();
 }
 
 pub fn is_key_valid(key_to_test: String, keys: Vec<String>) -> bool {
