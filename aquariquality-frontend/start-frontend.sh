579fd85eaffbcf104831d3a040d5d8442475309d
foxx <james.k@konnektive.com>
2024-02-14 11:23:49 -0500

    frontend

--- aquariquality-frontend/start-frontend.sh
+++ aquariquality-frontend/start-frontend.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd ./aquariquality-frontend/ || exit
 npm install
-npm run build
+#bash -c "npm run build"
 ./node_modules/next/dist/bin/next
--- src/frontend/mod.rs
+++ src/frontend/mod.rs
@@ -3,14 +3,14 @@ use std::thread;
 
 pub fn start_front_end() {
     thread::spawn(|| {
-        Command::new("pwd")
-            .spawn()
-            .unwrap();
-        // Command::new("bash")
-        //     .arg("-e")
-        //     .arg("aquariquality-frontend/start-frontend.sh")
+        // Command::new("pwd")
         //     .spawn()
         //     .unwrap();
+        Command::new("bash")
+            .arg("-e")
+            .arg("aquariquality-frontend/start-frontend.sh")
+            .spawn()
+            .unwrap();
     });
 }
 
