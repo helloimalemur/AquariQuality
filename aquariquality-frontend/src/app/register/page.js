da6d52b30ff0443b302ed4998e7568468cc1edaf
foxx <jkoontsiii@gmail.com>
2024-02-24 10:59:12 -0500

    .

--- aquariquality-frontend/src/app/register/page.js
+++ aquariquality-frontend/src/app/register/page.js
@@ -58,7 +58,7 @@ export default function Register() {
                   alt="AquariQuality"
               />
               <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-                Sign in to your account
+                Create an account
               </h2>
             </div>
             <form className="space-y-6" action="#" method="POST">
@@ -90,25 +90,7 @@ export default function Register() {
                 />
               </div>
 
-              <div className="flex items-center justify-between">
-                <div className="flex items-center">
-                  <input
-                      id="remember-me"
-                      name="remember-me"
-                      type="checkbox"
-                      className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
-                  />
-                  <label htmlFor="remember-me" className="ml-3 block text-sm leading-6 text-gray-900">
-                    Remember me
-                  </label>
-                </div>
 
-                <div className="text-sm leading-6">
-                  <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
-                    Forgot password?
-                  </a>
-                </div>
-              </div>
 
               <div>
                 <button
@@ -116,17 +98,10 @@ export default function Register() {
                     onClick={(e) => handleSubmit(e)}
                     className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 >
-                  Sign in
+                  Submit
                 </button>
               </div>
             </form>
-
-            <p className="text-center text-sm leading-6 text-gray-500">
-              Not a member?{' '}
-              <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
-                Register now
-              </a>
-            </p>
           </div>
         </div>
       </>
